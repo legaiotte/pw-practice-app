4451ecb8b78faff49f44bd71e919b5f7610e9c99
Leticia Gaiotte <147049973+legaiotte@users.noreply.github.com>
2026-01-19 18:21:03 +0000

    child elements

--- tests/fisrtTest.spec.ts
+++ tests/fisrtTest.spec.ts
@@ -8,7 +8,7 @@ import {test} from '@playwright/test'
 test.beforeEach(async({page}) => {
     await page.goto ('http://localhost:4200/')
     await page.getByText('Forms').click()
-    await page.getByText('Forms layouts').click()
+    await page.getByText('Form Layouts').click()
 })
 
 test('Locator syntax rules', async ({page}) => {
@@ -50,7 +50,16 @@ test('User facing locators', async({page}) => {
 
     await page.getByText('Using the Grid').click()
 
-    await page.getByTestId('').click()
+    await page.getByTestId('SignIn').click()
 
     await page.getByTitle('IoT Dashboard').click()
+})
+test('locating child elements', async({page}) =>{
+    //await page.locator('nb-card nb-radio: text-is("Option 1")').click()
+    await page.locator('nb-card').locator('nb-radio').locator(':text-is("Option 2")').click()
+
+    await page.locator('nb-card').getByRole('button', {name:"Sign in"}).first().click()
+
+    await page.locator('nb-card').nth(3).getByRole('button').click()
+
 })
