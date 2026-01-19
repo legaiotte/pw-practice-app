bcff9f226766609cf7947c848fdf78fbd6a7e164
Leticia Gaiotte <147049973+legaiotte@users.noreply.github.com>
2026-01-19 12:25:18 +0000

    hooks

--- tests/fisrtTest.spec.ts
+++ tests/fisrtTest.spec.ts
@@ -1,4 +1,14 @@
 import {test} from '@playwright/test'
+//hooks : optimize code flow and execution 
+// //beforeEachtest:executed for every test 
+//beforeAll:before all test execute this 
+//test.afterall: after all test nad afterEach:after each test
+//test.describe
+
+test.beforeEach(async({page}) => {
+    await page.goto ('http://localhost:4200/')
+    await page.getByText('Forms').click()
+})
 
 test('the first test', async ({page}) => {
     await page.goto('http://localhost:4200/')
@@ -7,10 +17,10 @@ test('the first test', async ({page}) => {
     
 })
 
-test('navigate to daepicker page', async ({page}) => {
+test('navigate to datepicker page', async ({page}) => {
     await page.goto('http://localhost:4200/')
     await page.getByText ('Forms').click()
-    await page.getByText('Forms Layouts').click()
+    await page.getByText('Datepicker').click()
     
 })
 
