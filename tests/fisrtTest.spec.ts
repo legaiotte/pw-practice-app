de453af9f699ff262741817097e67bbbf494eb5e
Leticia Gaiotte <147049973+legaiotte@users.noreply.github.com>
2026-01-19 13:31:08 +0000

    facing locators

--- tests/fisrtTest.spec.ts
+++ tests/fisrtTest.spec.ts
@@ -1,26 +1,45 @@
 import {test} from '@playwright/test'
-//hooks : optimize code flow and execution 
-// //beforeEachtest:executed for every test 
+//hooks: optimize code flow and execution 
+//beforeEachtest:executed for every test 
 //beforeAll:before all test execute this 
 //test.afterall: after all test nad afterEach:after each test
 //test.describe
 
 test.beforeEach(async({page}) => {
-    await page.goto ('http://localhost:4200/')
+    await page.goto ('http://localhost:4200')
     await page.getByText('Forms').click()
+    await page.getByText('Forms layouts').click()
 })
 
-test('the first test', async ({page}) => {
-    await page.goto('http://localhost:4200/')
-    await page.getByText ('Forms').click()
-    await page.getByText('Forms Layouts').click()
-    
+test('Locator syntax rules', async ({page}) => {
+    //by tag name
+    await page.locator('input').first().click()
+
+    //by ID
+    page.locator('#inputEmail1')
+
+    //by Class value
+    page.locator('.shape-retangle')
+
+    //by atribute
+    page.locator('[placeholder="Email"]')
+
+    //by class value (full)
+    page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]')
+
+    //combine different selectors
+    page.locator('input[placeholder="Email"][nbinput]')
+
+    //by xPath (not recommended)
+    page.locator('//*[@id="inputemail1"]')
+
+    //by partial text match
+    page.locator(':text("Using ")')
+
+    //by exact text match
+    page.locator(':text-is("Using the Grid")')
 })
 
-test('navigate to datepicker page', async ({page}) => {
-    await page.goto('http://localhost:4200/')
-    await page.getByText ('Forms').click()
-    await page.getByText('Datepicker').click()
-    
+test('User facing locators', async({page}) => {
+    page.getByRole('textbox', {name: "Email"}).first().click()
 })
-
